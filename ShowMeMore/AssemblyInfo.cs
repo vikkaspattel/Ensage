ccddc7876f2901d4cc6f578e7c8c6ee0180d4abb
dendimonster <patel.vikas34@gmail.com>
2015-11-30 14:50:55 +0530

    Update AssemblyInfo.cs

--- ShowMeMore/AssemblyInfo.cs
+++ ShowMeMore/AssemblyInfo.cs
@@ -1,1 +1,9 @@
 
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle ("ShowMeMore")]
+[assembly: AssemblyProduct ("ShowMeMore")]
+[assembly: ComVisible (false)]
+[assembly: AssemblyVersion ("1.0.0.0")]
+[assembly: AssemblyFileVersion ("1.0.0.0")]
