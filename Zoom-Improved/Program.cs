496171d80bea768177a577c4f38915dd22c5ed3a
dendimonster <patel.vikas34@gmail.com>
2015-12-29 19:27:53 +0530

    Update Program.cs

--- Zoom-Improved/Program.cs
+++ Zoom-Improved/Program.cs
@@ -7,11 +7,11 @@ namespace ZoomImproved
 	{
 		private static readonly Menu Menu = new Menu("zOOm", "zOOm", true);
 		private static readonly uint WM_MOUSEWHEEL = 0x020A;
-		private static readonly uint VK_CTRL = 0x11;
 		private static readonly ConVar ZoomVar = Game.GetConsoleVar("dota_camera_distance");
+		
 		static void Main()
 		{
-			var slider = new MenuItem("distance", "Default zOOm").SetValue(new Slider(1550, 1134, 2500));
+			var slider = new MenuItem("distance", "Distance Value").SetValue(new Slider(1550, 1134, 3000));
 			slider.ValueChanged += Slider_ValueChanged;
 			Menu.AddItem(slider);
 			Menu.AddToMainMenu();
@@ -19,28 +19,28 @@ namespace ZoomImproved
 			ZoomVar.SetValue(slider.GetValue<Slider>().Value);
 			Game.GetConsoleVar("r_farz").SetValue(18000);
 			Game.GetConsoleVar("fog_enable").SetValue(0);
+			Game.GetConsoleVar("dota_camera_disable_zoom").SetValue(1);
 			Game.OnWndProc += Game_OnWndProc;
 		}
+		
 		private static void Slider_ValueChanged(object sender, OnValueChangeEventArgs e)
 		{
 			ZoomVar.SetValue(e.GetNewValue<Slider>().Value);
 		}
+
 		private static void Game_OnWndProc(WndEventArgs args)
 		{
 			if (args.Msg == WM_MOUSEWHEEL && Game.IsInGame )
 			{
-				if (Game.IsKeyDown(VK_CTRL))
-				{
-					var delta = (short)((args.WParam >> 16) & 0xFFFF);
-					var zoomValue = ZoomVar.GetInt();
-					if (delta < 0)
-						zoomValue += 50;
-					else
-						zoomValue -= 50;
-					ZoomVar.SetValue(zoomValue);
-					Menu.Item("distance").SetValue(new Slider(zoomValue, 1134, 2500));
-					args.Process = false;
-				}
+				var delta = (short)((args.WParam >> 16) & 0xFFFF);
+				var zoomValue = ZoomVar.GetInt();
+				if (delta < 0)
+					zoomValue += 50;
+				else
+					zoomValue -= 50;
+				ZoomVar.SetValue(zoomValue);
+				Menu.Item("distance").SetValue(new Slider(zoomValue, 1134, 3000));
+				args.Process = false;
 			}
 		}
 	}
