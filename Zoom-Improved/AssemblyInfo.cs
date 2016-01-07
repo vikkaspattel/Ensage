462923ccc8a360abfaa16d79c95b30fde071763a
dendimonster <patel.vikas34@gmail.com>
2016-01-07 13:53:23 +0530

    Update AssemblyInfo.cs

--- Zoom-Improved/AssemblyInfo.cs
+++ Zoom-Improved/AssemblyInfo.cs
@@ -4,5 +4,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Zoom Improved")]
 [assembly: AssemblyProduct("Zoom Improved")]
+[assembly: AssemblyCopyright("Copyright © dendimonster 2015")]
+[assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.4.0.0")]
 [assembly: AssemblyFileVersion("1.4.0.0")]
