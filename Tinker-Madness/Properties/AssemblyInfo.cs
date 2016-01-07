8f6c3ff5b60d39478ca65c8cdea34d1d93a5d05a
dendimonster <patel.vikas34@gmail.com>
2016-01-07 18:02:16 +0530

    Update AssemblyInfo.cs

--- Tinker-Madness/Properties/AssemblyInfo.cs
+++ Tinker-Madness/Properties/AssemblyInfo.cs
@@ -4,5 +4,5 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Tinker Madness")]
 [assembly: AssemblyProduct("Tinker Madness")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
