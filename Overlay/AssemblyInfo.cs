3452a70bd561c10aabacd859bc7f5c47ea5d23c9
dendimonster <patel.vikas34@gmail.com>
2016-01-10 12:24:49 +0530

    Create AssemblyInfo.cs

---
+++ Overlay/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Overlay")]
+[assembly: AssemblyProduct("Overlay")]
+[assembly: AssemblyCopyright("Copyright © Dendimonster 2015")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
