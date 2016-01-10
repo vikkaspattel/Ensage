d40a1dd67d3cad7a876854ead63fca69381ae7da
dendimonster <patel.vikas34@gmail.com>
2016-01-10 22:30:22 +0530

    Delete AssemblyInfo.cs

--- Tinker-Madness/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Tinker Madness")]
-[assembly: AssemblyProduct("Tinker Madness")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
